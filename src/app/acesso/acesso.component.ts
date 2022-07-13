b4af27623fb130870b07ed3917f36aa05179c7bc
fabrizioabreu <fabrizioabreu@live.com>
2022-07-13 20:10:19 -0300

    Animations - Renderização condicional do login e do cadastro (nglf)

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -27,6 +27,8 @@ export class AcessoComponent implements OnInit {
   public estadoBanner: string = 'criado';
   public estadoPainel: string = 'criado';
 
+  public cadastro: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
