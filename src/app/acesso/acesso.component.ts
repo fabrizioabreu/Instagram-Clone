b9e66cf99ba499e37878a3d0c6a73c6482d27207
fabrizioabreu <fabrizioabreu@live.com>
2022-07-13 22:28:50 -0300

    Animations keyframes - Criando animação do painel (Login e Cadastro)

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -1,4 +1,4 @@
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { trigger, state, style, transition, animate, keyframes } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -17,7 +17,20 @@ import { Component, OnInit } from '@angular/core';
       state('criado', style({ opacity: 1 })),
       transition('void => criado', [ 
         style({ opacity:0, transform: 'translate(50px, 0)' }), 
-        animate('500ms 0s ease-in-out') // Duração, delay e aceleração
+        animate('1s 0s ease-in-out', // Duração, delay e aceleração
+        keyframes([
+          // offset: Marca o momento que o keyframe será incluido na animação ( de 0 a 1 )
+
+          // 0 void ----x--------------------------x--x--x----------x 1.5s //
+          style({ offset: 0.15, opacity: 1, transform: 'translateX(0    )' }),
+          style({ offset: 0.86, opacity: 1, transform: 'translateX(0    )' }),
+          style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.90, opacity: 1, transform: 'translateY(10px )' }),
+          style({ offset: 0.92, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.94, opacity: 1, transform: 'translateY(10px )' }),
+          style({ offset: 0.96, opacity: 1, transform: 'translateY(-10px)' }),
+          style({ offset: 0.98, opacity: 1, transform: 'translateY(10px )' })
+        ])) 
       ])
     ])
   ]
